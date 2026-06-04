18cfdce0dc57ee282640639ad7bddf98d56cdd6f
Stephen Gimpel <gimpelstephen1@gmail.com>
2026-06-03 20:17:06 -0700

    wip

---
+++ assign_1/sect_4/run_with_camera.sh
@@ -0,0 +1,4 @@
+./facial_rec \
+  --camera=0 \
+  --face_cascade=/usr/share/opencv4/haarcascades/haarcascade_frontalface_alt.xml \
+  --eyes_cascade=/usr/share/opencv4/haarcascades/haarcascade_eye_tree_eyeglasses.xml
---
+++ assign_1/sect_4/run_with_image.sh
@@ -0,0 +1,4 @@
+./facial_rec \
+  --image=./dr_siewert.png \
+  --face_cascade=/usr/share/opencv4/haarcascades/haarcascade_frontalface_alt.xml \
+  --eyes_cascade=/usr/share/opencv4/haarcascades/haarcascade_eye_tree_eyeglasses.xml
