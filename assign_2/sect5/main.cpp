7300e4c6e66efdec349c5cb43be30faf2c65e889
Stephen Gimpel <gimpelstephen1@gmail.com>
2026-06-14 12:22:01 -0700

    wip

--- assign_2/sect5/main.cpp
+++ assign_2/sect5/main.cpp
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
+#include <time.h>
 #include <unistd.h>
 
 #include <opencv2/core/core.hpp>
@@ -44,6 +45,13 @@ int main(int argc, char *argv[]) {
     cam0.set(CAP_PROP_FRAME_HEIGHT, WINDOW_HEIGHT);
     Mat mat_gray, dst, canny_edges, grad_x, grad_y, abs_grad_x, abs_grad_y;
     states current_state = NONE;
+
+    struct timespec fps_start, now;
+    clock_gettime(CLOCK_MONOTONIC, &fps_start);
+
+    int fps_frames = 0;
+    double fps = 0.0;
+
     while (1) {
         if ((winInput = waitKey(1)) == ESCAPE_KEY) {
             break;
@@ -63,7 +71,8 @@ int main(int argc, char *argv[]) {
 
         if (current_state == CANNY) {
             dst.create(frame.size(), frame.type());
-            blur(mat_gray, canny_edges, Size(3, 3)); // should blur match kerenal size?
+            // blur(mat_gray, canny_edges, Size(3, 3));
+            GaussianBlur(mat_gray, canny_edges, Size(3, 3), 0, 0, BORDER_DEFAULT);
             Canny(canny_edges, canny_edges, lowThreshold, lowThreshold * canny_ratio, kernel_size);
             dst.setTo(Scalar::all(0));
             frame.copyTo(dst, canny_edges);
@@ -77,6 +86,25 @@ int main(int argc, char *argv[]) {
             addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0, dst);
             frame = dst;
         }
+
+        fps_frames++;
+
+        clock_gettime(CLOCK_MONOTONIC, &now);
+
+        double elapsed =
+            (now.tv_sec - fps_start.tv_sec) +
+            (now.tv_nsec - fps_start.tv_nsec) / 1e9;
+
+        if (elapsed >= 1.0) {
+            fps = fps_frames / elapsed;
+
+            fps_frames = 0;
+            fps_start = now;
+        }
+
+        string fps_text = "FPS: " + to_string((int)fps);
+
+        putText(frame, fps_text, Point(10, 30), FONT_HERSHEY_SIMPLEX, 0.7, Scalar(255, 255, 255), 2);
 
         imshow("video_display", frame);
     }
